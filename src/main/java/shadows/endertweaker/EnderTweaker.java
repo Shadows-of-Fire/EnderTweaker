542d0acb001fbfa657999b297c64fa2c8c0bfa97
Shadows_of_Fire <Bward7864@gmail.com>
2019-02-02 16:29:38 -0500

    bump ver

--- src/main/java/shadows/endertweaker/EnderTweaker.java
+++ src/main/java/shadows/endertweaker/EnderTweaker.java
@@ -19,7 +19,7 @@ public class EnderTweaker {
 
 	public static final String MODID = "endertweaker";
 	public static final String MODNAME = "EnderTweaker";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.1.0";
 	public static final List<Runnable> ADDITIONS = new ArrayList<>();
 	public static final List<Runnable> REMOVALS = new ArrayList<>();
 
